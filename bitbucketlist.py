fbb40cf7380a070aedf11b05b31915e49f833be2
Sedthakit Prasanphanich <sedthakit.pra@truedigital.com>
2020-05-12 10:30:28 +0700

    Add more telemetry from the API

--- bitbucketlist.py
+++ bitbucketlist.py
@@ -1,8 +1,11 @@
 import requests
 import urllib.parse
+import json
+
 URL = "https://api.bitbucket.org/2.0/repositories?role=member&pagelen=100"
 HEADERS = {'content-type': 'application/json'}
 REPO_LIST = {}
+
 
 def main():
     """make the api to Bitbucket, TODO remove hardcoded secrets """
@@ -13,7 +16,7 @@ def main():
         response_list = api_call(URL)
         result_list = response_list['values']
         parse_result(result_list)
-
+        print(REPO_LIST)
 
         try:
             next_result = urllib.parse.unquote(response_list['next'])
@@ -29,9 +32,14 @@ def main():
             break
 
     with open('result.csv', 'w') as file:
-        file.write("repository, created on, last updated\n")
-        for repo, date in REPO_LIST.items():
-            file.write(f"{repo}, {date[0]}, {date[1]}\n")
+        file.write("repository, language, size, created on, last updated\n")
+        for key, value in REPO_LIST.items():
+            file.write(f"{key}, {value[0]}, {value[1]}, {value[2]}, {value[3]}\n")
+
+    print(json.dumps(REPO_LIST))
+
+    # with open('test_result.json', 'w') as file:
+    #     file.write(REPO_LIST)
 
 
 def api_call(conn):
@@ -44,7 +52,7 @@ def parse_result(result_list):
     """result is in the list format so we need to iterate to get the interesting information """
 
     for result in result_list:
-        REPO_LIST[result['full_name']] = [result['created_on'], result['updated_on']]
+        REPO_LIST[result['full_name']] = [result['language'], result['size'], result['created_on'], result['updated_on']]
 
 
 if __name__ == '__main__':
